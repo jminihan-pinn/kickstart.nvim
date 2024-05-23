439dc7ae8e383a70e6561ae71fe6360e6443d79a
jminihan <jminihan@pinnaclesolutionsinc.com>
2024-05-23 10:04:31 -0500

    Added tree view

--- init.lua
+++ init.lua
@@ -576,6 +576,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         tsserver = {},
+        omnisharp = {},
         --
 
         lua_ls = {
